fb5a5430a2ac7bde7f8f7780c01e080ab94e1f08
Tobias Andre Eggers <tobiasae@gmail.com>
2017-12-23 19:20:37 -0200

    dados do usuario

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Comment = (props) =>  
-    <p className="well">{props.comment.comment}</p>
+    <p className="well">
+    {props.comment.comment}
+        <b>por: {props.comment.user.name}</b>
+    </p>
     
 export default Comment
